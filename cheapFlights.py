43448ae359231226af905efc69a3c5a3c0fefc3b
Martinrolo <martinrolod@hotmail.com>
2024-05-18 21:14:49 -0400

    Removed unnecessary imports

--- cheapFlights.py
+++ cheapFlights.py
@@ -1,16 +1,14 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 import time
-import pandas as pd
-import smtplib
-from email.message import EmailMessage
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# from email.message import EmailMessage
 from flask import Flask, request, render_template 
   
 # Flask constructor
