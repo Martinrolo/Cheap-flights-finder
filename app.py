6135811b4414b2d2c7affec4e398891f032acda4
Martinrolo <martinrolod@hotmail.com>
2024-08-10 23:16:34 -0400

    Modified port

--- app.py
+++ app.py
@@ -13,5 +13,8 @@ app.register_blueprint(index_route)
 
 #Start the app        
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5001, threaded=True)
+
+
+#cloudflared tunnel --config C:\Users\marti\Desktop\Projets_persos\Test_Website\config.yml run 37621abe-519b-460b-8071-370715656ca0
 
